fb38509389506470b4be201328c21b5c4f34f311
patriciabarnabe <patbarnabe5@gmail.com>
2024-05-16 00:10:48 -0300

    Initial project configs and adding font families

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import { Inter, Dosis } from "next/font/google";
+import "./globals.css";
+import Image from "next/image";
+
+//Criando fontes variáveis que serão utilizadas em conjunto com o Tailwind no arquivo tailwind.config.js
+const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
+const dosis = Dosis({ subsets: ["latin"], variable: "--font-dosis" });
+
+export const metadata: Metadata = {
+  title: "Meta Diária",
+  description: "Gerenciador de Hábitos",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${dosis.variable} ${inter.variable} flex flex-col items-center mt-10 bg-neutral-900`}
+      >
+        <Image
+          src="/logo.svg"
+          alt="Logo checked - meta diária"
+          width={200}
+          height={200}
+        />
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  const habits = {};
+
+  return <main></main>;
+}
